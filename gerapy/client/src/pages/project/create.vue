388edc9327014a7c69c0e794658d288cdd468dcf
Germey <cqc@cuiqingcai.com>
2017-10-07 19:45:27 +0800

    add item

--- gerapy/client/src/pages/project/create.vue
+++ gerapy/client/src/pages/project/create.vue
@@ -3,10 +3,11 @@
   <div class="panel">
     <panel-title title="项目创建">
     </panel-title>
-    <div class="panel-body">
+    <div class="panel-body" id="project-create">
       <el-row>
         <el-col :span="24">
           <el-form ref="form" :model="configuration" label-width="100px">
+            <!-- 起始链接开始 -->
             <el-form-item>
               <h4 class="inline">起始链接</h4>
               <el-button type="primary" class="inline" size="mini" @click="onAddInput(configuration.startUrls)">
@@ -23,7 +24,10 @@
                 </el-button>
               </div>
             </el-form-item>
+            <!-- 起始链接结束 -->
+
             <div class="hr-line-dashed"></div>
+            <!-- 爬取规则开始 -->
             <el-form-item>
               <h4 class="inline">爬取规则</h4>
               <!-- 添加规则配置浮窗 -->
@@ -126,65 +130,168 @@
                 </el-collapse-item>
               </el-collapse>
             </el-form-item>
+            <!-- 爬取规则结束 -->
 
-            Extractors:
-            <div v-for="(extractor, extractorKey, extractorIndex) in configuration.extractors" :key="extractorKey">
-              Callback:
-              <el-input
-                v-model="extractor.callback" class="inline" placeholder="callback"
-                size="small"></el-input>
-              <div v-for="(value, key, index) in extractor.attrs" :key="key">
-                {{ key }}:
-                <el-button type="primary" size="mini" @click="onAddInput(extractor.attrs[key], {method: 'xpath'})">
-                  <i class="fa fa-plus"></i>
-                  添加
-                </el-button>
-                <el-button type="danger" size="mini"
-                           @click="onDeleteInput(extractor.attrs, key)">
-                  <i class="fa fa-remove"></i>
-                  删除
-                </el-button>
-                <div v-for="(v, k, i) in value" :key="k">
-                  <el-select v-model="v['method']" placeholder="请选择" size="small" class="inline">
-                    <el-option
-                      v-for="item in extractorMethods"
-                      :key="item.value"
-                      :label="item.label"
-                      :value="item.value">
-                    </el-option>
-                  </el-select>
-                  <el-input
-                    v-model="v['arg']" class="inline" placeholder="arg"
-                    size="small"></el-input>
-                  <el-input
-                    v-model="v['re']" class="inline" placeholder="re"
-                    size="small"></el-input>
-                  <el-button type="danger" size="mini"
-                             @click="onDeleteInput(value, k)">
-                    <i class="fa fa-remove"></i>
-                    删除
+            <div class="hr-line-dashed"></div>
+
+            <!-- 提取对象 -->
+            <el-form-item>
+              <h4 class="inline">提取对象</h4>
+              <!-- 添加规则配置浮窗 -->
+              <el-dialog :visible.sync="addItem" size="tiny">
+                <el-form>
+                  <el-form-item label="字段名称">
+                    <el-input size="small" v-model="item" class="inline" placeholder="字段名称">
+                    </el-input>
+                  </el-form-item>
+                </el-form>
+                <div slot="footer">
+                  <el-button @click="addItem=false" size="small">取消</el-button>
+                  <el-button @click="onAddItem()"
+                             type="primary" size="small">添加
                   </el-button>
                 </div>
-              </div>
-            </div>
-            Items:
-            <div v-for="(item, itemKey, itemIndex) in configuration.items" :key="itemKey">
-              Name:
-              <el-input
-                v-model="item['name']" class="inline" placeholder="callback"
-                size="small"></el-input>
-              <div v-for="(attr, attrKey, attrIndex) in item.attrs" :key="attrKey">
-                {{ attrKey }}
-                Input:
-                <el-input
-                  v-model="attr['inProcessor']" class="inline" placeholder="inProcessor"
-                  size="small"></el-input>
-                Output:
-                <el-input
-                  v-model="attr['outProcessor']" class="inline" placeholder="outProcessor"
-                  size="small"></el-input>
-              </div>
-            </div>
+              </el-dialog>
+              <!-- 添加规则配置浮窗 -->
+              <el-collapse accordion>
+                <el-collapse-item v-for="(item, itemKey, itemIndex) in configuration.items">
+                  <!-- 每个对象配置 -->
+                  <template slot="title">
+                    <span>
+                      对象{{ itemKey + 1 }}
+                    </span>
+                    <span class="pull-right">
+                      <el-button type="primary" class="inline" size="mini"
+                                 @click.stop="addItem=true,activeItem=itemKey">
+                        <i class="fa fa-plus"></i>
+                        添加
+                      </el-button>
+                      <el-button type="danger" size="mini" class="m-r-md"
+                                 @click="onDeleteInput(configuration.items, itemKey)">
+                          <i class="fa fa-remove"></i>
+                          删除
+                      </el-button>
+                    </span>
+                  </template>
+                  <!-- 每个对象配置 -->
+                  <el-form-item>
+                    名称
+                    <el-input
+                      v-model="item['name']" class="inline" placeholder="名称"
+                      size="small"></el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <div v-for="(attr, attrKey, attrIndex) in item.attrs" :key="attrKey" class="item">
+                      <el-button class="inline inline-first m-r-sm" type="primary" size="mini">
+                        {{ attrIndex + 1 }}
+                      </el-button>
+                      <span class="inline inline-second">{{ attrKey }}</span>
+                      <el-input
+                        v-model="attr['inProcessor']" class="inline inline-third" placeholder="输入处理"
+                        size="small"></el-input>
+                      <el-input
+                        v-model="attr['outProcessor']" class="inline inline-fourth" placeholder="输出处理"
+                        size="small"></el-input>
+                      <el-button type="danger" size="mini" class="m-r-md"
+                                 @click="onDeleteInput(item.attrs, attrKey)">
+                        <i class="fa fa-remove"></i>
+                        删除
+                      </el-button>
+                    </div>
+                  </el-form-item>
+                </el-collapse-item>
+              </el-collapse>
+            </el-form-item>
+            <!-- 提取对象结束 -->
+
+            <div class="hr-line-dashed"></div>
+
+
+            <!-- 提取规则开始 -->
+            <el-form-item>
+              <h4 class="inline">提取规则</h4>
+              <!-- 添加规则配置浮窗 -->
+              <el-dialog :visible.sync="addExtractorItem" size="tiny">
+                <el-form>
+                  <el-form-item label="选择规则配置">
+                    <el-select v-model="extractorItem" placeholder="请选择" size="small">
+                      <el-option
+                        v-for="item in extractorItemOptions"
+                        :key="item.value"
+                        :label="item.label"
+                        :value="item.value">
+                      </el-option>
+                    </el-select>
+                  </el-form-item>
+                </el-form>
+                <div slot="footer">
+                  <el-button @click="addExtractorItem=false" size="small">取消</el-button>
+                  <el-button @click="onAddExtractorItem()"
+                             type="primary" size="small">添加
+                  </el-button>
+                </div>
+              </el-dialog>
+              <!-- 添加规则配置浮窗 -->
+              <el-collapse accordion>
+                <el-collapse-item v-for="(extractor, extractorKey, extractorIndex) in configuration.extractors">
+                  <!-- 每条规则标题及操作配置 -->
+                  <template slot="title">
+                    <span>
+                      规则{{ extractorKey + 1 }}
+                    </span>
+                    <span class="pull-right">
+                      <el-button type="danger" size="mini" class="m-r-md"
+                                 @click="onDeleteInput(configuration.extractors, extractorKey)">
+                          <i class="fa fa-remove"></i>
+                          删除
+                      </el-button>
+                    </span>
+                  </template>
+                  <!-- 每条规则标题及操作配置 -->
+                  <el-form-item>
+                    <h5 class="inline m-v-sm">处理函数</h5>
+                    <el-input
+                      v-model="extractor.callback" class="inline" placeholder="处理函数名称"
+                      size="small">
+                    </el-input>
+                  </el-form-item>
+                  <el-form-item>
+                    <div v-for="(value, key, index) in extractor.attrs" :key="key">
+                      {{ key }}
+                      <el-button type="primary" size="mini"
+                                 @click="onAddInput(extractor.attrs[key], {method: 'xpath'})">
+                        <i class="fa fa-plus"></i>
+                        添加规则
+                      </el-button>
+                      <div v-for="(v, k, i) in value" :key="k" class="extractor-rule">
+                        <el-select v-model="v['method']" placeholder="提取方式" size="small" class="inline inline-first">
+                          <el-option
+                            v-for="item in extractorMethods"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                          </el-option>
+                        </el-select>
+                        <el-input
+                          v-model="v['arg']" class="inline inline-second" placeholder="提取规则"
+                          size="small"></el-input>
+                        <el-input
+                          v-model="v['re']" class="inline inline-third" placeholder="正则表达式"
+                          size="small"></el-input>
+                        <el-button type="danger" size="mini"
+                                   @click="onDeleteInput(extractor.attrs, key, k)">
+                          <i class="fa fa-remove"></i>
+                          删除
+                        </el-button>
+                      </div>
+                    </div>
+                  </el-form-item>
+                </el-collapse-item>
+              </el-collapse>
+            </el-form-item>
+            <!-- 提取规则结束 -->
+
+
           </el-form>
         </el-col>
       </el-row>
@@ -195,6 +302,8 @@
 <script type="text/javascript">
   import {panelTitle, bottomToolBar} from 'components'
   import ElCollapseItem from "../../../node_modules/element-ui/packages/collapse/src/collapse-item";
+  import ElFormItem from "../../../node_modules/element-ui/packages/form/src/form-item";
+  import ElInput from "../../../node_modules/element-ui/packages/input/src/input";
   export default{
     data(){
       return {
@@ -287,6 +396,11 @@
             value: 'attr',
             label: 'Attr'
           }],
+
+        addItem: false,
+        activeItem: null,
+        item: null,
+
         configuration: {
           startUrls: [
             "http://www.baidu.com",
@@ -321,6 +435,7 @@
           extractors: [
             {
               callback: 'parse_item',
+              item: 'NewsItem',
               attrs: {
                 title: [
                   {
@@ -342,18 +457,15 @@
             {
               name: 'NewsItem',
               attrs: {
-                title: [
-                  {
-                    inProcessor: 'dssdf',
-                    outProcessor: 'sdfsdf',
-                  }
-                ],
-                url: [
-                  {
-                    inProcessor: 'dssdf',
-                    outProcessor: 'sdfsdf',
-                  }
-                ]
+                title: {
+                  inProcessor: 'dssdf',
+                  outProcessor: 'sdfsdf',
+                },
+                url: {
+                  inProcessor: 'dssdf',
+                  outProcessor: 'sdfsdf',
+                }
+
               }
             }
           ]
@@ -362,6 +474,8 @@
       }
     },
     components: {
+      ElInput,
+      ElFormItem,
       ElCollapseItem,
       panelTitle,
       bottomToolBar
@@ -387,6 +501,10 @@
       onAddRuleItem() {
         this.$set(this.configuration.rules[this.activeRule], this.ruleItem, this.ruleItemInit[this.ruleItem])
         this.addRuleItem = false
+      },
+      onAddItem() {
+        this.$set(this.configuration.items[this.activeItem]['attrs'], this.item, {})
+        this.addItem = false
       }
     }
   }
@@ -403,15 +521,41 @@
     padding: 20px;
   }
 
-  .el-form-item__content {
-    margin-left: 10px !important;
+  #project-create {
+    .el-form-item__content {
+      margin-left: 10px !important;
+    }
+
+    .el-collapse-item__wrap {
+      background-color: white !important;
+    }
+    h4, h5 {
+      font-weight: 200;
+    }
+    .extractor-rule {
+      .inline-first {
+        width: 100px;
+      }
+      .inline-second {
+        width: calc(100% - 380px);
+      }
+      .inline-third {
+        width: 200px;
+      }
+    }
+    .item {
+      .inline-first {
+        min-width: 16px;
+      }
+      .inline-second {
+        width: 60px;
+        text-align: center;
+      }
+      .inline-third, .inline-fourth {
+        width: calc((100% - 200px) / 2);
+      }
+    }
   }
 
-  .el-collapse-item__wrap {
-    background-color: white !important;
-  }
 
-  h4, h5 {
-    font-weight: 200;
-  }
 </style>
