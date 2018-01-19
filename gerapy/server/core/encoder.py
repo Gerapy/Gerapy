2cc6eed7e72f431268a580f1c84f8a03fe6c28b9
Germey <cqc@cuiqingcai.com>
2018-01-19 12:31:46 +0800

    add build encoder of Client Model

--- gerapy/server/core/encoder.py
+++ gerapy/server/core/encoder.py
@@ -5,11 +5,13 @@ import decimal
 import json
 import uuid
 from django.db.models import QuerySet
+from django.forms import model_to_dict
 from django.utils import six
 from django.utils.duration import duration_iso_string
 from django.utils.functional import Promise
 from django.utils.timezone import is_aware
 from django.utils import timezone
+from gerapy.server.core.models import Client
 from gerapy.server.core.time import DATE_TIME_FORMAT
 
 
@@ -20,6 +22,8 @@ class JSONEncoder(json.JSONEncoder):
     
     def default(self, o):
         # See "Date Time String Format" in the ECMA-262 specification.
+        print('Build Type', type(o))
+        
         if isinstance(o, datetime.datetime):
             return timezone.localtime(o).strftime(DATE_TIME_FORMAT)
         elif isinstance(o, datetime.date):
@@ -41,5 +45,7 @@ class JSONEncoder(json.JSONEncoder):
             return six.text_type(o)
         elif isinstance(o, QuerySet):
             return list(o.values())
+        elif isinstance(o, Client):
+            return model_to_dict(o)
         else:
             return super(JSONEncoder, self).default(o)
