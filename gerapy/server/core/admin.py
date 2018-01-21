48705441f3d9e4c2e5a09382b9bcce51c43b655d
xiaohan <95028555@qq.com>
2018-01-21 13:25:19 +0800

    一:语言修改成中文 二:关闭时区配置,原因是如果支持时区,在用model.__month...这样的写法时查出来的数据会有问题， 三:修改django自带后台的一些配置，添加搜索

--- gerapy/server/core/admin.py
+++ gerapy/server/core/admin.py
@@ -3,18 +3,23 @@ from django.contrib import admin
 # Register your models here.
 from gerapy.server.core.models import Client, Project, Monitor
 
+admin.site.site_header = '爬虫后台管理'
+admin.site.site_title = '爬虫后台管理'
 
+
+@admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'ip', 'port', 'created_at', 'updated_at')
+    list_display = ['name', 'ip', 'port', 'created_at', 'updated_at']
+    search_fields = ['name', 'ip']
 
 
+@admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'configurable', 'built_at', 'generated_at')
+    list_display = ['name', 'description', 'configurable', 'built_at', 'generated_at']
+    search_fields = ['name']
 
 
+@admin.register(Monitor)
 class MonitorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'description', 'project', 'created_at', 'updated_at')
-
-admin.site.register(Client, ClientAdmin)
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Monitor, MonitorAdmin)
+    list_display = ['name', 'description', 'project', 'created_at', 'updated_at']
+    search_fields = ('name',)
--- gerapy/server/server/settings.py
+++ gerapy/server/server/settings.py
@@ -104,17 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
+LANGUAGE_CODE = 'zh-hans'
 
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'PRC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
+USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
