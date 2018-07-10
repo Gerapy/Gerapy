ebeedbd73bb2812f9c8bd8d2ffa74bc73980aff8
Germey <cqc@cuiqingcai.com>
2018-07-11 02:46:36 +0800

    rm upload

--- gerapy/client/src/components/index.js
+++ gerapy/client/src/components/index.js
@@ -1,15 +1,11 @@
-
-
 import leftSlide from 'components/leftSlide'
 import mainContent from 'components/mainContent'
 import panelTitle from 'components/panelTitle'
-import simpleImageUpload from 'components/simpleImageUpload'
 import bottomToolBar from 'components/bottomToolBar'
 
 export {
   leftSlide,
   mainContent,
   panelTitle,
-  simpleImageUpload,
   bottomToolBar,
 }
