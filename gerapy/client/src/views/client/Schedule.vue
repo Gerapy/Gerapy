320229053cfae65a3d52df3417a05d345796bfd0
Germey <cqc@cuiqingcai.com>
2019-08-30 14:49:46 +0800

    update shedule

--- gerapy/client/src/views/client/Schedule.vue
+++ gerapy/client/src/views/client/Schedule.vue
@@ -2,14 +2,11 @@
 	<div>
 		<div class="panel" v-for="project in projects" :key="project">
 			<panel-title :title="project">
-				<el-button @click.stop="onRefresh" size="small">
-					<i class="fa fa-refresh"></i>
-				</el-button>
 			</panel-title>
-			<div class="panel-body" v-loading="projectsLoadData">
+			<div class="panel-body" v-loading="projectsLoading">
 				<el-table
 					:data="spiders[project]"
-					v-loading="spidersLoadData[project]"
+					v-loading="spidersLoading[project]"
 					:style="{width: '100%'}">
 					<el-table-column
 						prop="id"
@@ -24,7 +21,7 @@
 					<el-table-column
 						:label="$lang.columns.operations">
 						<template slot-scope="props">
-							<el-button type="success" size="mini" @click="startSpider(project, props.row.name)">
+							<el-button type="success" size="mini" @click="onStartSpider(project, props.row.name)">
 								<i class="fa fa-caret-right"></i>
 								{{ $lang.buttons.run }}
 							</el-button>
@@ -33,33 +30,36 @@
 				</el-table>
 				<el-collapse accordion @change="getLog">
 					<el-collapse-item v-for="job in jobs[project]" :name="job.id" :key="job.id">
-						<template slot-scope="title">
-              <span v-if="job.spider">
+						<template slot="title">
+              <span v-if="job.spider" class="m-l-xs" :style="{minWidth: '120px'}">
                 <i class="fa fa-bug"></i>
                 {{ $lang.columns.spiderName }}:
                 {{ job.spider }}
               </span>
-							<span v-if="job.spider" class="m-l-md">
+							<span v-if="job.spider" class="m-l-md" :style="{minWidth: '290px'}">
                 <i class="fa fa-key"></i>
                 {{ $lang.columns.jobID }}:
                 {{ job.id }}
               </span>
-							<span v-if="job.start_time" class="m-l-md">
+							<span v-if="job.start_time" class="m-l-md" :style="{minWidth: '190px'}">
                 <i class="el-icon-time"></i>
                 {{ $lang.columns.startTime }}:
                 {{ job.start_time.substring(0, 16) }}
               </span>
-							<span v-if="job.end_time" class="m-l-md">
+							<span v-if="job.end_time" class="m-l-md" :style="{minWidth: '190px'}">
                 <i class="el-icon-time"></i>
                 {{ $lang.columns.endTime }}:
                 {{ job.end_time.substring(0, 16) }}
               </span>
-							<span class="wrapper">
-                <el-button :type="jobStatusClass[job.status]" size="mini" class="pull-right m-t-sm m-r-md">
+							<span class="m-l-md">
+                <el-button :type="jobStatusClass[job.status]" size="mini" class="pull-right m-r-md">
+									<i v-if="['pending'].includes(job.status)" class="fa fa-circle-thin"></i>
+									<i v-if="['running'].includes(job.status)" class="fa fa-spin fa-spinner"></i>
+									<i v-if="['finished'].includes(job.status)" class="fa fa-check"></i>
                   {{ jobStatusText[job.status] }}
                 </el-button>
-                <el-button type="danger" size="mini" class="pull-right m-t-sm m-r-md"
-													 v-if="['pending', 'running'].includes(job.status)" @click.stop="cancelJob(job.id)">
+                <el-button type="danger" size="mini" class="pull-right m-r-md"
+													 v-if="['pending', 'running'].includes(job.status)" @click.stop="onCancelJob(job.id)">
                   <i class="fa fa-remove"></i>
                   <span v-if="['pending'].includes(job.status)">
                     {{ $lang.buttons.cancel }}
@@ -70,7 +70,7 @@
                 </el-button>
               </span>
 						</template>
-						<div v-loading="logLoadData" :element-loading-text="logLoadDataText">
+						<div v-loading="logLoading" :element-loading-text="logLoadingText">
 							<pre>{{ logs[job.id] }}</pre>
 						</div>
 					</el-collapse-item>
@@ -81,18 +81,18 @@
 </template>
 <script>
 	import PanelTitle from '../../components/PanelTitle'
-	
+
 	export default {
 		data() {
 			return {
 				// 所有项目
 				projects: [],
 				// 加载projects标志
-				projectsLoadData: true,
+				projectsLoading: true,
 				// 所有爬虫 {'project1': ['spider1', 'spider2'], 'project2': ['spider1']}
 				spiders: {},
 				// 加载spiders标志
-				spidersLoadData: {},
+				spidersLoading: {},
 				// 所有任务 {'project1': [{'id': '', 'spider': '', 'status': ''}]}
 				jobs: {},
 				// 任务状态
@@ -114,13 +114,13 @@
 				// 日志信息 {'logid': 'text'}
 				logs: {},
 				// 加载logs标志
-				logLoadData: true,
+				logLoading: true,
 				// 加载log提示语
-				logLoadDataText: this.$store.getters.$lang.messages.loading,
+				logLoadingText: this.$store.getters.$lang.messages.loading,
 				// 定时刷新log指针
-				logLoadDataInterval: null,
+				logLoadingInterval: null,
 				// 标记正在加载哪个log
-				logLoadDataActive: null,
+				logLoadingActive: null,
 				// 路由信息
 				routeId: this.$route.params.id
 			}
@@ -140,144 +140,130 @@
 		methods: {
 			//获取所有项目
 			getProjects() {
-				this.$fetch.apiClient
-					.projects({
-						id: this.routeId
+				this.$http.get(this.format(this.$store.state.url.client.projects, {
+					id: this.routeId
+				})).then(({data: data}) => {
+					this.projects = data
+					this.projectsLoading = false
+					this.projects.forEach(project => {
+						this.$set(this.spidersLoading, project, true)
 					})
-					.then(({data: data}) => {
-						this.projects = data
-						this.projectsLoadData = false
-						this.projects.forEach(project => {
-							this.$set(this.spidersLoadData, project, true)
-						})
-						// 获取所有爬虫
-						this.getSpiders()
-						// 获取所有任务
-						this.getJobs()
-					})
-					.catch(() => {
-						//this.$message.error('获取项目失败，正在重试')
-						this.projectsLoadData = false
-						this.getProjects()
-					})
+					// 获取所有爬虫
+					this.getSpiders()
+					// 获取所有任务
+					this.getJobs()
+				}).catch(() => {
+					this.projectsLoading = false
+					// 定时任务
+					this.$store.commit(
+						'setTimeout',
+						setTimeout(() => {
+							this.getProjects()
+						}, 3000)
+					)
+				})
 			},
 			// 获取所有爬虫
 			getSpiders() {
 				this.projects.forEach(project => {
-					this.$fetch.apiClient
-						.listSpiders({
-							id: this.routeId,
-							project: project
-						})
-						.then(({data: data}) => {
-							this.$set(this.spiders, project, data)
-							this.$set(this.spidersLoadData, project, false)
-						})
-						.catch(() => {
-							this.$set(this.spidersLoadData, project, false)
-						})
+					this.$http.get(this.format(this.$store.state.url.client.listSpiders, {
+						id: this.routeId,
+						project: project
+					})).then(({data: data}) => {
+						this.$set(this.spiders, project, data)
+						this.$set(this.spidersLoading, project, false)
+					}).catch(() => {
+						this.$set(this.spidersLoading, project, false)
+					})
 				})
 			},
 			// 获取所有任务
 			getJobs() {
 				this.projects.forEach(project => {
-					this.$fetch.apiClient
-						.listJobs({
-							id: this.routeId,
-							project: project
-						})
-						.then(({data: data}) => {
-							this.$set(this.jobs, project, data)
-							for (let project in this.jobs) {
-								let jobs = this.jobs[project]
-								jobs.forEach(job => {
-									this.$set(this.jobsInfo, job.id, {
-										project: project,
-										spider: job['spider']
-									})
+					this.$http.get(this.format(this.$store.state.url.client.listJobs, {
+						id: this.routeId,
+						project: project
+					})).then(({data: data}) => {
+						this.$set(this.jobs, project, data)
+						for (let project in this.jobs) {
+							let jobs = this.jobs[project]
+							jobs.forEach(job => {
+								this.$set(this.jobsInfo, job.id, {
+									project: project,
+									spider: job['spider']
 								})
-							}
-						})
+							})
+						}
+					})
 				})
 			},
 			// 启动任务
-			startSpider(project, spider) {
-				this.$fetch.apiClient
-					.startSpider({
-						id: this.routeId,
-						project: project,
-						spider: spider
-					})
-					.then(() => {
-						this.$message.success(
-							this.$store.getters.$lang.messages.successRun
-						)
-						this.getJobs()
-					})
-					.catch(() => {
-						this.$message.error(
-							this.$store.getters.$lang.messages.errorRun
-						)
-					})
+			onStartSpider(project, spider) {
+				this.$http.get(this.format(this.$store.state.url.client.startSpider, {
+					id: this.routeId,
+					project: project,
+					spider: spider
+				})).then(() => {
+					this.$message.success(
+						this.$store.getters.$lang.messages.successRun
+					)
+					this.getJobs()
+				}).catch(() => {
+					this.$message.error(
+						this.$store.getters.$lang.messages.errorRun
+					)
+				})
 			},
 			// 获取日志
 			getLog(job) {
 				// 如果展开日志
 				if (job) {
 					//设置活跃日志
-					this.logLoadDataActive = job
+					this.logLoadingActive = job
 					// 正在加载
-					this.logLoadData = true
+					this.logLoading = true
 					// 请求日志
-					this.$fetch.apiClient
-						.getLog({
-							id: this.routeId,
-							project: this.jobsInfo[this.logLoadDataActive]['project'],
-							spider: this.jobsInfo[this.logLoadDataActive]['spider'],
-							job: this.logLoadDataActive,
-							random: Math.random()
-						})
-						.then(({data: data}) => {
-							//设置日志字典
-							this.$set(this.logs, this.logLoadDataActive, data)
-							// 加载日志完成
-							this.logLoadData = false
-							// 定时任务
-							this.$store.commit(
-								'setTimeout',
-								setTimeout(() => {
-									this.getLog(job)
-								}, 2000)
-							)
-						})
-						.catch(() => {
-							// 如果错误
-							this.logLoadData = false
-						})
+					this.$http.get(this.format(this.$store.state.url.client.getLog, {
+						id: this.routeId,
+						project: this.jobsInfo[this.logLoadingActive]['project'],
+						spider: this.jobsInfo[this.logLoadingActive]['spider'],
+						job: this.logLoadingActive,
+						random: Math.random()
+					})).then(({data: data}) => {
+						//设置日志字典
+						this.$set(this.logs, this.logLoadingActive, data)
+						// 加载日志完成
+						this.logLoading = false
+						// 定时任务
+						this.$store.commit(
+							'setTimeout',
+							setTimeout(() => {
+								this.getLog(job)
+							}, 2000)
+						)
+					}).catch(() => {
+						this.logLoading = false
+					})
 				} else {
 					//闭合之后停止加载日志
 					this.$store.commit('clearTimeout')
 				}
 			},
-			cancelJob(job) {
-				this.$fetch.apiClient
-					.cancelJob({
-						id: this.routeId,
-						project: this.jobsInfo[job]['project'],
-						job: job
-					})
-					.then(() => {
-						this.$message.success(
-							this.$store.getters.$lang.messages.canceling
-						)
-						this.getJobs()
-					})
-					.catch(() => {
-						//如果错误
-						this.$message.error(
-							this.$store.getters.$lang.messages.errorCancel
-						)
-					})
+			onCancelJob(job) {
+				this.$http.get(this.format(this.$store.state.url.client.cancelJob, {
+					id: this.routeId,
+					project: this.jobsInfo[job]['project'],
+					job: job
+				})).then(() => {
+					this.$message.success(
+						this.$store.getters.$lang.messages.canceling
+					)
+					this.getJobs()
+				}).catch(() => {
+					this.$message.error(
+						this.$store.getters.$lang.messages.errorCancel
+					)
+				})
 			}
 		}
 	}
--- gerapy/client/vue.config.js
+++ gerapy/client/vue.config.js
@@ -1,15 +1,18 @@
 module.exports = {
-  outputDir: '../server/core/templates',
-  assetsDir: 'static',
-  devServer: {
-    host: '0.0.0.0',
-    hot: true,
-    proxy: {
-      '/api/*': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      },
-    }
-  }
+	outputDir: '../server/core/templates',
+	assetsDir: 'static',
+	devServer: {
+		host: '0.0.0.0',
+		hot: true,
+		proxy: {
+			'/api/*': {
+				target: 'http://localhost:5000',
+				changeOrigin: true,
+				secure: false
+			},
+		}
+	},
+	css: {
+		sourceMap: true
+	}
 }
