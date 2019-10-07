45223a60e3efb810f815fe63b261000f8af92914
thsheep <thsheep@live.com>
2019-10-08 02:02:35 +0800

    add http interceptors

---
+++ gerapy/client/src/http.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+import router from './router'
+
+import { getToken, removeToken } from './utils/auth'
+
+/* eslint-disable */
+
+
+
+axios.defaults.timeout = 8000;
+
+axios.interceptors.request.use(
+    config => {
+        console.log("设置 Header")
+        if (getToken()){
+            config.headers.Authorization = 'Token ' + getToken()
+
+        }
+
+        return config
+    },
+
+    error => {
+        return Promise.reject(error)
+    }
+)
+
+axios.interceptors.response.use(
+    response => {
+        if (response.status === 401) {
+            router.push({path: '/login'});
+            console.log("Token Error");
+        }else if (response.status === 403) {
+            router.push({path: '/home'});
+            console.log("No Permission");
+        }
+        return response;
+    },
+    error => {
+        return Promise.reject(error);
+    }
+);
+
+
+export default axios
